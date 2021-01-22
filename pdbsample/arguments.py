af6be93a04e85bf3ad8486fe952ff5b52f718a3e
Paul Bond <git@paulsbond.co.uk>
2021-01-22 14:36:35 +0000

    Added the bin_entries argument

--- pdbsample/arguments.py
+++ pdbsample/arguments.py
@@ -6,23 +6,26 @@ def parse():
     parser.add_argument(
         "--res-bins",
         type=int,
-        metavar="N",
         default=10,
         help="Number of resolution bins (default: 10)",
     )
     parser.add_argument(
         "--res-max",
         type=float,
-        metavar="X",
         default=3.5,
         help="Maximum resolution (exclusive) (default: 3.5)",
     )
     parser.add_argument(
         "--res-min",
         type=float,
-        metavar="X",
         default=1.0,
         help="Minimum resolution (inclusive) (default: 1.0)",
+    )
+    parser.add_argument(
+        "--bin-entries",
+        type=int,
+        default=2,
+        help="Number of entries per resolution bin (default: 2)",
     )
     args = parser.parse_args()
     args.res_step = (args.res_max - args.res_min) / args.res_bins
